08f3f79e378571401e4e1628ceef94ba114e5eae
Nicolò Boschi <boschi1997@gmail.com>
2024-02-21 15:40:21 +0100

    add grafana

---
+++ grafana/setup-db.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker exec -it postgres psql -U postgres -c 'drop table if exists tests'
+docker exec -it postgres psql -U postgres -c 'create table if not exists tests (run_id varchar(50) PRIMARY KEY, LLM varchar(100), latency float, tokens integer,  time timestamp not null, error boolean, eval varchar(2000))'
+docker cp runs.csv postgres:/tmp/runs.csv
+docker exec -it postgres psql -U postgres -c "copy tests from '/tmp/runs.csv' delimiter ',' csv header"
+docker exec -it postgres psql -U postgres -c "select count(*) from tests"
